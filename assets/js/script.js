a6004e8e28c6da069025bd2315e48b275a03220e
Shuai Zuo <szuo1@vols.utk.edu>
2022-05-03 16:06:29 -0500

    Handle Form Submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; 
-    listItemEl.textContent = "This is a new task."; 
-    tasksToDoEl.appendChild(listItemEl); 
-}; 
+var createTaskHandler = function(event) {
+    event.preventDefault();
   
-buttonEl.addEventListener("click", createTaskHandler);
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+};
+
+formEl.addEventListener("submit", createTaskHandler);
